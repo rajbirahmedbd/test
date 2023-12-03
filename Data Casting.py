a0ace917c3aecacca5ee89101163c5e2733e4928
rajbirahmedbd <rajbirahmed@gmail.com>
2023-12-04 00:05:19 +0600

    initial commit

---
+++ Data Casting.py
@@ -0,0 +1,12 @@
+number_of_eggs = input('How many eggs you want to buy: ')
+eggs_number = int(number_of_eggs)
+print(type(number_of_eggs),type(eggs_number))
+print(eggs_number*15)
+
+price = '11.45'
+price_float = float(price)
+print(type(price),type(price_float))
+
+roll = 95
+roll_str = str(roll)
+print(type(roll),type(roll_str))
